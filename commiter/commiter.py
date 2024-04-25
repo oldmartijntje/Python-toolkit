a80130259cb1b25f2c69b556d3eea804d4c908e3
Martijn <oldmartijntje@gmail.com>
2024-04-25 21:24:55 +0200

    Add sleep functionality to auto commit feature in commiter.py

--- commiter/commiter.py
+++ commiter/commiter.py
@@ -10,6 +10,7 @@ class GitAutoCommitApp:
         self.master.title("Git Auto Commit")
         
         self.commit_interval = 10  # Default interval in minutes
+        self.commit_sleep = 15
         self.repository_path = "C:\\Users\\oldma\\AppData\\Roaming\\.betacraft\\Update\\saves"
         self.running = False
         
@@ -23,16 +24,23 @@ class GitAutoCommitApp:
         self.interval_entry = ttk.Entry(self.master)
         self.interval_entry.grid(row=1, column=1, padx=10, pady=5)
         self.interval_entry.insert(0, self.commit_interval)
+
+        self.sleep_label = ttk.Label(self.master, text="Sleep before recheck (seconds):")
+        self.sleep_label.grid(row=4, column=0, padx=10, pady=5)
+        
+        self.sleep_entry = ttk.Entry(self.master)
+        self.sleep_entry.grid(row=4, column=1, padx=10, pady=5)
+        self.sleep_entry.insert(0, self.commit_sleep)
         
         self.start_button = ttk.Button(self.master, text="Start", command=self.start_auto_commit)
-        self.start_button.grid(row=4, column=0, columnspan=1, padx=10, pady=5)
+        self.start_button.grid(row=5, column=0, columnspan=1, padx=10, pady=5)
 
         self.stop_button = ttk.Button(self.master, text="Stop", command=self.stop_auto_commit)
-        self.stop_button.grid(row=4, column=1, columnspan=1, padx=10, pady=5)
+        self.stop_button.grid(row=5, column=1, columnspan=1, padx=10, pady=5)
         self.stop_button.config(state="disabled")
 
         self.close_button = ttk.Button(self.master, text="Exit", command=self.exit)
-        self.close_button.grid(row=5, column=0, columnspan=1, padx=10, pady=5)
+        self.close_button.grid(row=6, column=0, columnspan=1, padx=10, pady=5)
 
         # create 2 text boxes + lable
         # nr 1 is for the path to the git repo
@@ -54,14 +62,23 @@ class GitAutoCommitApp:
         self.time_label = ttk.Label(self.master, text="Last commit: XXXX-XX-XX XX:XX:XX")
         self.time_label.grid(row=0, column=1, padx=10, pady=5)
 
+        self.tick_label = ttk.Label(self.master, text="Auto Commit: OFF")
+        self.tick_label.grid(row=0, column=2, padx=10, pady=5)
+
+        self.restart_button = ttk.Button(self.master, text="Restart", command=self.restart)
+        self.restart_button.grid(row=5, column=2, columnspan=1, padx=10, pady=5)
+        self.restart_button.config(state="disabled")
+
         self.commit_button = ttk.Button(self.master, text="Commit Now", command=self.commit)
-        self.commit_button.grid(row=4, column=2, columnspan=1, padx=10, pady=5)
+        self.commit_button.grid(row=6, column=2, columnspan=1, padx=10, pady=5)
 
         self.checkbox2_var = tk.BooleanVar()
         self.checkbox2 = ttk.Checkbutton(self.master, text="Commit On Exit", variable=self.checkbox2_var)
-        self.checkbox2.grid(row=5, column=1, padx=10, pady=10)
+        self.checkbox2.grid(row=6, column=1, padx=10, pady=10)
 
-        
+    def restart(self):
+        self.stop_auto_commit()
+        self.start_auto_commit()
 
     def start_auto_commit(self):
         if not self.running:
@@ -70,6 +87,7 @@ class GitAutoCommitApp:
                 self.running = True
                 self.start_button.config(state="disabled")
                 self.stop_button.config(state="normal")
+                self.restart_button.config(state="normal")
                 self.auto_commit_thread = threading.Thread(target=self.auto_commit)
                 self.auto_commit_thread.start()
             except ValueError:
@@ -82,18 +100,25 @@ class GitAutoCommitApp:
             self.running = False
             self.start_button.config(state="normal")
             self.stop_button.config(state="disabled")
+            self.restart_button.config(state="disabled")
+            self.tick_label.config(text="Auto Commit: OFF")
 
     def auto_commit(self):
         loop = 0
         while self.running:
-            loop += 1   
+            slep = self.sleep_entry.get()
+            if (slep.isnumeric() == False):
+                self.sleep_entry.delete(0, tk.END)
+                self.sleep_entry.insert(0, str(self.commit_sleep))
+                slep = self.commit_sleep
+            slep = int(slep)
+            loop += slep 
+            self.tick_label.config(text=str(loop) + "/" + str(self.commit_interval * 60))
             if self.checkbox_var.get():
                 if loop >= self.commit_interval  * 60:
                     self.commit()
                     loop = 0
-            else:
-                print("nope")
-            time.sleep(1)
+            time.sleep(slep)
 
     def commit(self):
         try:
